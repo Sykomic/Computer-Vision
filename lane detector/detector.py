2b9b0cc3db65f2ca799659a65e89b1af2c1f20eb
Daesik Choi <seafood0917@gmail.com>
2020-08-14 20:29:02 +0900

    Lane detecting with hough (Not fully understanded yet)

--- lane detector/detector.py
+++ lane detector/detector.py
@@ -24,15 +24,59 @@ def do_segment(frame):
 
 def calculate_lines(frame, lines):
     # TODO
-    return None
+    # Empty arrays to store the coordinates of the left and right lines
+    left = []
+    right = []
+    # Loops through every detected line
+    for line in lines:
+        # Reshapes line from 2D array to 1D array
+        x1, y1, x2, y2 = line.reshape(4)
+        # Fits a linear polynomial to the x and y coordinates and returns a vector
+        # of coefficients which describe the slope and y-intercept
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        y_intercept = parameters[1]
+        # If slope is negative, the line is to the left of the lane, and otherwise,
+        # the line is to the right of the lane
+        if slope < 0:
+            left.append((slope, y_intercept))
+        else:
+            right.append((slope, y_intercept))
+    # Averages out all the values for the left and right into a single slope and
+    # y-intercept value for each line
+    left_avg = np.average(left, axis=0)
+    right_avg = np.average(right, axis=0)
+    # Calculates the x1, y1, x2, y2 coordinates for the left and right lines
+    left_line = calculate_coordinates(frame, left_avg)
+    right_line = calculate_coordinates(frame, right_avg)
+
+    return np.array([left_line, right_line])
 
 def calculate_coordinates(frame, parameters):
     # TODO
-    return None
+    slope, intercept = parameters
+    # Sets initial y-coordinate as height from top down (bottom of the frame)
+    y1 = frame.shape[0]
+    # Sets final y-coordinate as 150 above the bottom of the frame
+    y2 = int(y1 - 150)
+    # Sets initial x-coordinate as (y1 - b) / m since y1 = mx1 + b
+    x1 = int((y1 - intercept) / slope)
+    # Sets final x-coordinate as (y2 - b) / m since y2 = mx2 + b
+    x2 = int((y2 - intercept) / slope)
+
+    return np.array([x1, y1, x2, y2])
 
 def visualize_lines(frame, lines):
     # TODO
-    return None
+    # Creates an image filled with zero intensities with the same dimensions as
+    # the frame
+    lines_visualize = np.zeros_like(frame)
+    # Checks if any lines are detected
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            # Draws lines between two coordinate with green color and 5 thickness
+            cv.line(lines_visualize, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    return lines_visualize
 
 # TODO
 # The video feed is read in as a VideoCapture object
@@ -42,8 +86,20 @@ while (cap.isOpened()):
                             # frame = the current frame being projected in the video.
     canny = do_canny(frame)
     segment = do_segment(canny)
+    hough = cv.HoughLinesP(segment, 2, np.pi / 180, 100, np.array([]), minLineLength = 100,
+    maxLineGap = 50)
 
-    cv.imshow('input', segment)
+    # Averages multiple detected lines from hough into one line for left border of lane
+    # and one line for right border fo lane
+    lines = calculate_lines(frame, hough)
+    # visualizes the lines
+    lines_visualize = visualize_lines(frame, lines)
+    # Overlays lines on frame by taking their weighted sums and adding an arbitrary
+    # scalar value of 1 as the gamma argument
+    output = cv.addWeighted(frame, 0.9, lines_visualize, 1, 1)
+    # Opens a new window and displays the output frame
+
+    cv.imshow('output', output)
     if cv.waitKey(10) & 0xFF == ord('q'): # Frames are read by intervals of 10 milliseconds.
         break                            # The programse breaks out of the while loop when the user presses the 'q' key
 
